c22da5a78cb350ae59e127c83a03379d168e41bd
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-08-02 00:59:42 -0400

    first commit

---
+++ api/api.py
@@ -0,0 +1,69 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+import re
+from person import *
+
+
+def get_page(location):
+    file = open(location, "r")
+    page = file.read()
+    file.close()
+    return page
+
+app = Flask(__name__)
+
+
+# Flow for adding user to line
+#
+# QR code routes to
+
+@app.route('/')
+def route_to_new_person():
+    return redirect("/new_person.html", code=302)
+
+@app.route('/new_person.html')
+def new_person():
+    page = get_page("client/new_person.html")
+    return page
+
+# Adds person to line
+@app.route("/create_new_person.py", methods=['POST', 'GET'])
+def create_new_person():
+    phone = request.args.get("phone_num")
+    phone = str(re.sub(r'\D', '', phone))
+
+    new_user = Person(phone=phone)
+    new_user.add_to_line()
+    return redirect(f"/display_person.html?id={new_user.id}", code=302)
+
+@app.route('/display_person.html')
+# returns order in line
+def display_person():
+    user = Person(id=int(request.args.get("id")))
+    page = get_page("C:/Users/willi/OneDrive/Desktop/In_Line/client/display_person.html")
+    page = page.format(id=str(user.id))
+    return page
+
+# Backend endpoints for htmx to access
+#
+#
+"""
+@app.route('/person/phone')
+def get_phone():
+    user = Person(id=int(request.args.get("id")))
+    user.link_to_database()
+    return user.phone
+"""
+    
+@app.route('/person/phone')
+def get_phone():
+    user = Person(id=int(request.args.get("id")))
+    return user.phone
+
+@app.route('/person/place')
+def get_place():
+    user = Person(id=int(request.args.get("id")))
+    return str(user.calculate_place())
+app.run(port=5500, debug=True)
---
+++ api/person.py
@@ -0,0 +1,84 @@
+import pandas as pd
+
+
+class Person:
+    def __init__(self, id=None, in_line=None, phone=None, position=None):
+        self.df = pd.read_csv("C:/Users/willi/OneDrive/Desktop/In_Line/api/line.csv", header=0)
+        self.df.set_index('id', inplace=True)
+
+        self.id = id
+        self.in_line = in_line
+        self.phone = phone
+        self.position = position
+
+        if self.id is None:
+            if self.df.index.empty:
+                self.id = 0
+            else:
+                self.id = int(self.df.index[-1]) + 1
+            
+        else:
+            # If we don't know if he is in line
+            if self.in_line is None:
+                if not pd.isna(self.df['in_line'].loc[self.id]):
+                    if self.df['in_line'].loc[self.id] == "True":
+                        self.in_line = True
+                    elif self.df['in_line'].loc[self.id] == "False":
+                        self.in_line = False
+
+            # If no phone number looks for one in database
+            if self.phone is None:
+                if not pd.isna(self.df['phone'].loc[self.id]):
+                    self.phone = str(self.df['phone'].loc[self.id])
+        
+        # Updates the csv with info from the database
+        self.df.loc[self.id] = {'id': self.id, 'in_line': True, 'phone': self.phone}
+        self.df.to_csv("C:/Users/willi/OneDrive/Desktop/In_Line/api/line.csv", index=True)
+    
+        self.df = pd.read_csv("C:/Users/willi/OneDrive/Desktop/In_Line/api/line.csv", header=0)
+        self.df.set_index('id', inplace=True)
+
+    def link_to_database(self):
+        # If no id, generates one
+        if self.id is None:
+            if self.df.index.empty:
+                self.id = 0
+            else:
+                self.id = int(self.df.index[-1]) + 1
+            
+        else:
+            # If we don't know if he is in line
+            if self.in_line is None:
+                if not pd.isna(self.df['in_line'].loc[self.id]):
+                    if self.df['in_line'].loc[self.id] == "True":
+                        self.in_line = True
+                    elif self.df['in_line'].loc[self.id] == "False":
+                        self.in_line = False
+
+            # If no phone number looks for one in database
+            if self.phone is None:
+                if not pd.isna(self.df['phone'].loc[self.id]):
+                    self.phone = str(self.df['phone'].loc[self.id])
+        
+        # Updates the csv with info from the database
+        self.df.loc[self.id] = {'id': self.id, 'in_line': True, 'phone': self.phone}
+        self.df.to_csv("C:/Users/willi/OneDrive/Desktop/In_Line/api/line.csv", index=True)
+
+    def add_to_line(self):
+        self.in_line = True
+        self.link_to_database()
+
+    def remove_from_line(self):
+        self.in_line = False
+        self.link_to_database()
+
+    def calculate_place(self):
+        pos = -1
+        for value in self.df['in_line']:
+            pos += 1
+            if value:
+                self.position = self.id - pos
+                return self.position
+                break
+
+
